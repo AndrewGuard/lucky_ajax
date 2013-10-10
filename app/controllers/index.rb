5f655d33c68f12dd556d91a8b09db6b9441bef3d
Andrew Guard <andrew.s.guard@gmail.com>
2013-10-10 10:06:07 -0500

    real version

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,8 +2,6 @@ get '/' do
   erb :index
 end
 
-
-# TODO: convert this route to use AJAX
 post '/rolls' do
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
@@ -11,5 +9,11 @@ post '/rolls' do
 
   @roll = value ? Roll.create({ value: value }) : Roll.create
 
-  erb :index  # HINT: what does this do? what should we do instead?
+   if request.xhr? 
+        puts '*************************************************in'
+
+    erb :rolled_die, layout: false
+  else
+    erb :index
+  end
 end
